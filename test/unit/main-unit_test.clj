55ecdf7b50d1e08e929f185ee85d30da789b2bc6
Rosiney Gomes Pereira <rosiney.gp@gmail.com>
2020-08-13 23:11:32 +0000

    test: include test for str-slug

--- test/unit/main-unit_test.clj
+++ test/unit/main-unit_test.clj
@@ -4,10 +4,28 @@
 (load-file-without-hashbang "src/includes/colors.clj")
 (load-file-without-hashbang "src/main.clj")
 
-
 (deftest "str-date-time"
   (testing "return type"
     (is (= (string? (str-date-time)) true)))
 
   (testing "time format (lenght)"
     (is (= (count (seq (str-date-time))) 19))))
+
+(deftest "str-slug"
+  (testing "to lower case"
+    (is (= (str-slug "UPPERCASE") "uppercase")))
+
+  (testing "replace :"
+    (is (= (str-slug "c:l:e:a:n") "c-l-e-a-n")))
+
+  (testing "replace ."
+    (is (= (str-slug "c.l.e.a.n") "c-l-e-a-n")))
+
+  (testing "replace /"
+    (is (= (str-slug "c/l/e/a/n") "c-l-e-a-n")))
+
+  (testing "replace _"
+    (is (= (str-slug "c_l_e_a_n") "c-l-e-a-n")))
+
+  (testing "replace spaces"
+    (is (= (str-slug "c l e a n") "c-l-e-a-n"))))
