3fc04ed04f627e60a05cdec50cae73f407ff9237
Rosiney Gomes Pereira <rosiney.gp@gmail.com>
2020-08-14 01:04:32 +0000

    feat: include test for fn branch-or-tag-name

--- test/unit/main-unit_test.clj
+++ test/unit/main-unit_test.clj
@@ -4,6 +4,8 @@
 (load-file-without-hashbang "src/includes/colors.clj")
 (load-file-without-hashbang "src/main.clj")
 
+(use ["sleep"])
+
 (deftest "str-date-time"
   (testing "return type"
     (is (= (string? (str-date-time)) true)))
@@ -29,3 +31,55 @@
 
   (testing "replace spaces"
     (is (= (str-slug "c l e a n") "c-l-e-a-n"))))
+
+(deftest "branch-or-tag-name"
+  (testing "local branch [dev]"
+    (is (= (branch-or-tag-name) "dev")))
+    
+  (env "GITHUB_ACTIONS" "true")
+  (env "GITHUB_REF" "/refs/heads/github-actions")
+
+  (testing "github actions"
+    (is (= (branch-or-tag-name) "github-actions")))
+  
+  (unset "GITHUB_ACTIONS")
+  (unset "GITHUB_REF")
+
+  (env "GITLAB_CI" "true")
+  (env "CI_COMMIT_REF_NAME" "gitlab-ci")
+
+  (testing "gitlab-ci"
+    (is (= (branch-or-tag-name) "gitlab-ci")))
+
+  (unset "GITLAB_CI")
+  (unset "CI_COMMIT_REF_NAME")
+
+  (env "JENKINS_URL" "http://jenkins")
+  (env "GIT_BRANCH" "jenkins")
+  
+  (testing "jenkins"
+    (is (= (branch-or-tag-name) "jenkins")))
+
+  (unset "JENKINS_URL")
+  (unset "GIT_BRANCH")
+
+  (env "TRAVIS" "true")
+  (env "TRAVIS_BRANCH" "travis")
+
+  (testing "travis"
+    (is (= (branch-or-tag-name) "travis")))
+
+  (unset "TRAVIS")
+  (unset "TRAVIS_BRANCH")
+
+  (env "CIRCLECI" "true")
+  (env "CIRCLE_TAG" "circle-ci-tag")
+  (env "CIRCLE_BRANCH" "circle-ci-brach")
+
+  (testing "circle-ci-tag"
+    (is (= (branch-or-tag-name) "circle-ci-tag")))
+
+  (unset "CIRCLE_TAG")
+
+  (testing "circle-ci-branch"
+    (is (= (branch-or-tag-name) "circle-ci-brach"))))
