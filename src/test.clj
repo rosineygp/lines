a9d3bc4f902bdd30df66a96e26cc7333b1e823c5
Rosiney Gomes Pereira <rosiney.gp@gmail.com>
2020-08-14 01:03:52 +0000

    fix: remove time elapsed

--- src/test.clj
+++ src/test.clj
@@ -10,24 +10,22 @@
       (if (= @exit-code 0)
         (do
           (println (str "\033[32m[ " name  " ]\033[0m")
-            " asserts: " @asserts "/" (- @asserts @exit-code) 
-            ", time elapsed: " (- (time-ms) start) "ms\n")
+                   " asserts: " @asserts "/" (- @asserts @exit-code) "\n")
           (reset! asserts 0))
         (do
           (println (str "\033[31m[ " name  " ]\033[0m")
-            " asserts: " @asserts "/" (- @asserts @exit-code) 
-            ", time elapsed: " (- (time-ms) start) "ms\n")
+                   " asserts: " @asserts "/" (- @asserts @exit-code) "\n")
           (exit! @exit-code))))))
 
 (defn testing [msg f]
   (do
     (swap! asserts inc)
     (println (if f
-                (do
-                  (str "\033[32m[   ok   ]\033[0m"))
-                (do
-                  (swap! exit-code inc)
-                  (str "\033[31m[ failed ]\033[0m"))) msg)))
+               (do
+                 (str "\033[32m[   ok   ]\033[0m"))
+               (do
+                 (swap! exit-code inc)
+                 (str "\033[31m[ failed ]\033[0m"))) msg)))
 
 (defn is [comparison]
   (= comparison true))
