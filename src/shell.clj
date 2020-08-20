517c936786201d052170b4485a9d0750a090a1af
Rosiney Gomes Pereira <rosiney.gp@gmail.com>
2020-08-20 20:22:35 +0000

    feat: shell execution

--- src/shell.clj
+++ src/shell.clj
@@ -1,26 +1,45 @@
 (defn lines-shell-sudo [job]
-    (if (= (get job :sudo) true)
-        "sudo"
-        ""))
+  (if (= (get job :sudo) true)
+    "sudo"
+    ""))
 
 (defn lines-shell-exec [job command]
   (let [sudo (lines-shell-sudo job)
-        result (sh! (str sudo " " command))]
+        result (sh! (apply str-join " " [(apply str-join " " (if (get job :entrypoint)
+                                                               (get job :entrypoint)
+                                                               ["bash" "-c"]))
+                                        "'"
+                                        (str "BRANCH_NAME=" (branch-or-tag-name))
+                                        (if (get job :variables)
+                                          (apply str-join " " (map
+                                                     (fn [key]
+                                                       (str key "=" (get (get job :variables) key)))
+                                                     (keys (get job :variables)))) "")
+                                         ";"
+                                         sudo
+                                         command 
+                                         "'"]))]
     (do
       (print-command result)
-      (if (> (nth result 2) 0)
-        (throw (str "exit-code " (nth result 2)))
-        result))))
+      (lines-throw-command result))))
 
 (defn lines-shell-job [job]
-  (do
-    (try*
-     (map (fn [command]
-            (do
-              (output-line-action (str "shell: " (magenta (lines-shell-sudo job)) " " (white command)))
-              (lines-shell-exec job command)))
-          (get job :script))
-     (catch* ex
-             (let [exit-code (last (str-split ex " "))]
-               (do
-                 (println (bg-red (white (bold "JOB FAILED"))))))))))
+  
+    (do
+      (println @retries)
+      (try*
+      (map (fn [command]
+              (do
+                (output-line-action (apply str-join " " ["shell:"
+                                                        (magenta (lines-shell-sudo job))
+                                                        (white command)]))
+                (lines-shell-exec job command)))
+            (get job :script))
+      (catch* ex
+              (let [tt "e"]
+                (do
+                  (println (lines-throw-split ex))
+                  (swap! retries dec)
+                  (lines-shell-job job)
+                  
+                  (println (bg-red (white (bold "JOB FAILED"))))))))))
