9ccde7e0178413e96358b52b227a4eb3d3eb89fd
Rosiney Gomes Pereira <rosiney.gp@gmail.com>
2020-08-26 22:01:54 -0300

    feat: print full job and tasks

--- src/pretty-print.clj
+++ src/pretty-print.clj
@@ -5,16 +5,16 @@
   (nth (date ["--date"
               (str "@" (timemillis-to-epoch time))]) 0))
 
-(defn lines-pp-cmd [script-line]
-(let [std (get script-line :stdout)
-        err (get script-line :stderr)
-        exit-code (get script-line :exit-code)]
-    (println (get script-line :raw-cmd))
-    (if (= (empty? std) false) (println std))
-    (if (= (empty? err) false) (println-stderr (red err)))
-    (if (number? exit-code) (println-stderr (magenta exit-code)))))
+(defn lines-pp-cmd [line]
+  (str (green "cmd: ") (get line :raw-cmd) "\n"
+       (if (= (empty? (get line :stderr)) false) (str (red (get line :stderr) "\n")) "")
+       (if (= (empty? (get line :stdout)) false) (str (get line :stdout) "\n") "")
+       (str (red "exit-code: ") (get line :exit-code) "\n"
+            (blue "time: ") (- (get line :finished) (get line :start)) " ms\n")))
 
 (defn lines-pp-default [result]
-  (println (get result :name))
-  (println (timemillis-to-date (get result :start)))
-  (map lines-pp-cmd (get result :script)))
+  (println
+   (str (magenta "job: ") (get result :name) "\n"
+        (cyan "start: ") (timemillis-to-date (get result :start)) "\n\n"
+        (apply str-join "\n" (map lines-pp-cmd (get result :script))) "\n"
+        (cyan "finished: ") (timemillis-to-date (get result :finished)))))
