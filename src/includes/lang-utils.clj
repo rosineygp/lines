5557afe6b8f64dcc1253a22453cb04406cc58e71
Rosiney Gomes Pereira <rosiney.gp@gmail.com>
2020-09-13 19:14:48 -0300

    fix: remove swap from or function

--- src/includes/lang-utils.clj
+++ src/includes/lang-utils.clj
@@ -1,9 +1,7 @@
 (defn or [& i]
-  (let [r (atom 0)]
-    (do
-      (map (fn [x]
-             (if (= x true) (swap! r inc) false)) i)
-      (if (> @r 0) true false))))
+  (let [n (map (fn [a] (if (= a true) 1 0)) i)
+        r (reduce (fn [a b] (+ a b)) 0 n)]
+    (if (> r 0) true false)))
 
 (defn hashmap-list [l]
   (map (fn [k] (vector k (get l k))) (keys l)))
