c2c89dc919ee22a9d3a10c8db6b21358befb92cc
Rosiney Gomes Pereira <rosiney.gp@gmail.com>
2020-09-18 16:10:44 -0300

    chore: move handler functions to core file

--- src/main.clj
+++ src/main.clj
@@ -21,28 +21,6 @@
 (def ttl 3600)
 (def max-attempts 2)
 
-(defn lines-job-status [l]
-  (let [exit-sum (reduce (fn [a b] (+ a b)) 0 l)]
-    (if (= exit-sum 0) true false)))
-
-(defn lines-retries [r]
-  (if r (if (> r max-attempts) max-attempts r) 0))
-
-(defn lines-job-retry-inner [retries k! f job]
-  (let [r ((call f) job)
-        retry? (reduce (fn [a b] (+ a b)) 0 (map (fn [x] (get x :exit-code)) r))]
-    (do
-      (swap! k! concat [r])
-      (if (> retry? 0)
-        (if (> retries 0)
-          (lines-job-retry-inner (dec retries) k! f job))))))
-    
-(defn lines-job-retry [retries f job]
-  (let [k (atom [])]
-    (do
-      (lines-job-retry-inner retries k f job)
-      @k)))
-
 (defn job [item]
   (let [start (time-ms)
         method (get item :method)
