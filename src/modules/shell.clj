a5ce7cfc5499635b313bd790dca0d41748adb8de
Rosiney Gomes Pereira <rosiney.gp@gmail.com>
2020-09-19 16:10:52 -0300

    chore: rmeove commented lines

--- src/modules/shell.clj
+++ src/modules/shell.clj
@@ -31,95 +31,11 @@
                          (str-escapes script-index)
                          "'"])))
 
-; (defn lines-shell-exec [job script-index]
-;   (let [start (time-ms)
-;         sudo (str-shell-sudo job)
-;         branch (branch-or-tag-name)
-;         cmd  (str-shell-command-line job script-index)
-;         result (sh! cmd)
-;         finished (time-ms)]
-;     (do
-;       (lines-throw-command result)
-;       (lines-task-obj start
-;                       finished
-;                       (nth result 0)
-;                       (nth result 1)
-;                       (nth result 2)
-;                       script-line
-;                       cmd))))
-
-; (defn lines-shell-exec [job script-index]
-;   (let [start (time-ms)
-;         sudo (str-shell-sudo job)
-;         branch (branch-or-tag-name)
-;         cmd  (str-shell-command-line job script-index)
-;         finished (time-ms)]
-;       (println cmd)
-;        (cmd)))
-
-; (defn lines-job-method [job retries]
-;   (let [method (get job :method)]
-;     (do
-;       (if (> @retries 0)
-;         (try*
-;           (do ((call (str "lines-" method "-job")) job ))
-;          (catch* ex
-;                  (do
-;                    (swap! retries dec)
-;                    (lines-output-error (str "Command failed: " @retries "/" max-attempts))
-;                    (lines-job-method job retries))))
-;         (throw "exit-code=1,message=Error in all attempts.")))))
-
-; (defn job [item]
-;   (let [start (time-ms)
-;         method (get item :method)
-;         retries (if (get item :retries)
-;                   (atom (if (> (get item :retries) max-attempts) max-attempts (get item :retries)))
-;                   (atom 1))
-;         tasks (try*
-;                (lines-job-method item retries)
-;                (catch* ex
-;                        (let [error (lines-throw-split ex)]
-;                          (throw (str "exit-code=" (get error :exit-code) ",message=Job " (get item :name) " failed.")))))
-;         pipestatus (map (fn [x] (get x :exit-code)) tasks)
-;         result (assoc item
-;                       :start start
-;                       :finished (time-ms)
-;                       :pipestatus pipestatus
-;                       :success (lines-job-status pipestatus)
-;                       :tasks tasks)]
-;     (do
-;       (println result)
-;       result)))
-
-
-
-; :allow_failure true
 (defn lines-tasks-allow-failure [allow_failure]
   (if allow_failure true false))
 
-
-; (defn lines-tasks-retries [j]
-;   )
-
 (defn lines-tasks-break [result]
   (if (> (get result :exit-code) 0) (throw result) result))
-
-; https://stackoverflow.com/questions/12068640/retrying-something-3-times-before-throwing-an-exception-in-clojure
-
-; (defn retry-inner [retries cmd k!]
-;   (let [r (lines-task-execute cmd)]
-;     (do
-;       (swap! k! concat [r])
-;       (if (> (get r :exit-code) 0)
-;         (if (> retries 0)
-;           (retry-inner (dec retries) cmd k!))))))
-  
-; (defn retry [retries cmd]
-;   (let [k (atom [])]
-;     (do
-;       (retry-inner retries cmd k)
-;       @k)))
 
 (defn lines-task-execute [cmd]
   (let [start (time-ms)
