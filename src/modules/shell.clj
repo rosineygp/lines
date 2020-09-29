d7e6272e2ceb2a16cc52166819d1966778228a50
Rosiney Gomes Pereira <rosiney.gp@gmail.com>
2020-09-29 20:57:32 -0300

    feat: run shell commands locally and remotely

--- src/modules/shell.clj
+++ src/modules/shell.clj
@@ -1,3 +1,20 @@
+(defn str-connection-ssh [c]
+  (let [user (if (get c :user) (get c :user) (env "USER"))
+        port (if (get c :port) (get c :port) 22)
+        common (apply str-join " " ["ssh"
+                                    (str user "@" (get c :host))
+                                    "-p"
+                                    port])]
+    (cond
+      (key-exist? c :key) (apply str-join " " [common
+                                               "-i"
+                                               (get c :key)])
+      (key-exist? c :password) (apply str-join " " ["sshpass"
+                                                    "-p"
+                                                    (str "'" (get c :password) "'")
+                                                    common])
+      (keyword? :else) common)))
+
 (defn str-shell-sudo [job]
   (let [user (if (get job :user) (get job :user) "root")
         sudo? (if (= (get job :sudo) true) true false)]
@@ -19,8 +36,11 @@
   (apply str-join " " (if (nil? entrypoint) ["bash" "-c"] entrypoint)))
 
 (defn str-shell-command-line [job script-index]
-  (let [branch (branch-or-tag-name)]
-    (apply str-join " " [(str-shell-sudo job)
+  (let [branch (branch-or-tag-name)
+        remote? (if (get job :connection) (if (= (get (get job :connection) :method) "local") false true) false)
+        method (if remote? (if (get (get job :connection) :method) (get (get job :connection) :method) "ssh"))]
+    (apply str-join " " [(if remote? (str ((call (str "str-connection-" method)) (get job :connection)) " \"") "")
+                         (str-shell-sudo job)
                          (str-shell-entrypoint (get job :entrypoint))
                          "$'"
                          "export"
@@ -29,7 +49,8 @@
                          (str-shell-job-variables (get job :variables))
                          ";"
                          (str-escapes script-index)
-                         "'"])))
+                         "'"
+                         (if remote? "\"" "")])))
 
 (defn lines-job-shell [job]
   (lines-task-loop job str-shell-command-line))
