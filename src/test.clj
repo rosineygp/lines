c228fac78a2f765d3704afa3963c13d7aab1ca1d
Rosiney Gomes Pereira <rosiney.gp@gmail.com>
2020-10-05 22:35:02 -0300

    chore: move function is to bottom

--- src/test.clj
+++ src/test.clj
@@ -3,9 +3,6 @@
 (def asserts (atom 0))
 (def exit-code (atom 0))
 
-(defn is [comparison]
-  (= comparison true))
-
 (defn deftest [title & l]
   (let [total (count l)
         success (reduce (fn [a b] (+ a b)) 0 (map (fn [x] (if (= (get x :result) true) 1 0)) l))
@@ -14,7 +11,6 @@
     (do
       (println "[" title "]" (str success "/" total) ":total-time" total-time "ms\n")
       (if (= success total) l (exit! 1)))))
-  
 
 (defmacro! testing
   (fn* [name body]
@@ -27,3 +23,6 @@
               (= result true)  (println "\033[32m[   ok   ]\033[0m " ~name ":time" time-elapsed "ms")
               (= result false) (println "\033[31m[ failed ]\033[0m " ~name ":time" time-elapsed "ms" "\n  " '~body))
             (hash-map :name ~name :start start :result result :end end :time-elapsed time-elapsed)))))
+
+(defn is [comparison]
+  (= comparison true))
