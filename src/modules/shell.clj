cab2a3ce2b0f5aef3ae3def6f57150d49dda297c
Rosiney Gomes Pereira <rosiney.gp@gmail.com>
2021-01-05 07:11:48 -0300

    fix: different behavior between local and remote execution

--- src/modules/shell.clj
+++ src/modules/shell.clj
@@ -22,22 +22,18 @@
               (keys vars))) ""))
 
 (defn str-shell-entrypoint [entrypoint]
-  (str-cmd (if (nil? entrypoint) ["bash" "-c"] entrypoint)))
+  (str-cmd (if (nil? entrypoint) ["bash" "-s"] entrypoint)))
 
 (defn str-shell-command-line [job script-index]
   (let [branch (branch-or-tag-name)
-        remote? (isremote? job)
         method (let [k (get-in job [:target :method])] (if (string? k) k "ssh"))]
-    (str-cmd [(if remote? (str ((call (str "str-target-" method)) (get job :target)) " \"") "")
+    (str-cmd [(if (isremote? job) ((call (str "str-target-" method)) (get job :target)) "")
               (str-shell-sudo job)
               (str-shell-entrypoint (get-in job [:args :entrypoint]))
-              "$'"
-              "export"
-              (str-shell-job-vars (get job :vars))
-              ";"
-              (str-escapes script-index)
-              "'"
-              (if remote? "\"" "")])))
+              "<<-'LINES-BLOCK-EOF'\n"
+              "export" (str-shell-job-vars (get job :vars)) ";\n"
+              script-index
+              "\nLINES-BLOCK-EOF"])))
 
 (defn lines-module-shell [job]
   (lines-task-loop job str-shell-command-line))
