abd049578e58454e35fdb8cb90eefacab08117c6
Rosiney Gomes Pereira <rosiney.gp@gmail.com>
2021-01-17 10:06:15 -0300

    refacor: expand macros for colors

--- src/includes/colors.clj
+++ src/includes/colors.clj
@@ -1,39 +1,41 @@
-(defn colors-escape-code
-  [i]
-  (str "\033[" i "m"))
+(def! colors-escape-code
+  (fn* [i]
+       (str "\033[" i "m")))
 
-(defn colors-reset [] (colors-escape-code 0))
+(def! colors-reset (fn* [] (colors-escape-code 0)))
 
-(def colors-code {gray    30
-                  red     31
-                  green   32
-                  yellow  33
-                  blue    34
-                  magenta 35
-                  cyan    36
-                  white   37
-                  bg-gray    40
-                  bg-red     41
-                  bg-green   42
-                  bg-yellow  43
-                  bg-blue    44
-                  bg-magenta 45
-                  bg-cyan    46
-                  bg-white   47
-                  bold          1
-                  dark          2
-                  underline     4
-                  blink         5
-                  reverse-color 7
-                  concealed     8})
+(def! colors-code {gray    30
+                   red     31
+                   green   32
+                   yellow  33
+                   blue    34
+                   magenta 35
+                   cyan    36
+                   white   37
+                   bg-gray    40
+                   bg-red     41
+                   bg-green   42
+                   bg-yellow  43
+                   bg-blue    44
+                   bg-magenta 45
+                   bg-cyan    46
+                   bg-white   47
+                   bold          1
+                   dark          2
+                   underline     4
+                   blink         5
+                   reverse-color 7
+                   concealed     8})
 
-(defn colors-make [l]
-  (pmap (fn [color]
-          (quasiquote (defn ~(symbol color) [string]
-                        (str
-                         (colors-escape-code (get colors-code ~color))
-                         string
-                         (colors-reset))))) l))
+(def! colors-make
+  (fn* [l]
+       (pmap (fn* [color]
+                  (quasiquote (def! ~(symbol color)
+                                (fn* [string]
+                                     (str
+                                      (colors-escape-code (get colors-code ~color))
+                                      string
+                                      (colors-reset)))))) l)))
 
 (map eval (colors-make ["gray"
                         "red"
